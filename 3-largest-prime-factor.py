b0cab7083d947618b7152b95068dce270b0dfd28
Daw-Ran Liou <dawran6@gmail.com>
2016-12-22 16:54:12 -0800

    extract out prime_factors function

--- 3-largest-prime-factor.py
+++ 3-largest-prime-factor.py
@@ -1,13 +1,4 @@
-def prime_factors(num):
-    i = 2
-    while i * i <= num:
-        if num % i:
-            i += 1
-        else:
-            num //= i
-            yield i
-    if num > 1:
-        yield num
+from utils import prime_factors
 
 if __name__ == '__main__':
     f = prime_factors(600851475143)
--- 5-smallest-multiple.py
+++ 5-smallest-multiple.py
@@ -1,17 +1,8 @@
 from collections import Counter, defaultdict
 from functools import reduce
 from operator import mul
+from utils import prime_factors
 
-def prime_factors(num):
-    i = 2
-    while i * i <= num:
-        if num % i:
-            i += 1
-        else:
-            num //= i
-            yield i
-    if num > 1:
-        yield num
 
 if __name__ == '__main__':
     d = defaultdict(int)
