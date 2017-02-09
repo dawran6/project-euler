5dd48acb2f56de72187ae1d47ccd404eb244202c
Daw-Ran Liou <dawran6@gmail.com>
2017-02-09 11:44:08 -0800

    Add new answer to #2

--- 2-even-fibonacci-numbers.py
+++ 2-even-fibonacci-numbers.py
@@ -1,3 +1,7 @@
+from utils import fibonacci_gen
+import itertools
+import timeit
+
 def fib_gen(num=10000):
     n_minus_2 = 0
     n_minus_1 = 1
@@ -7,9 +11,24 @@ def fib_gen(num=10000):
         n_minus_2 = n_minus_1
         n_minus_1 = n
 
-if __name__ == '__main__':
+def solve_1():
     fibs = fib_gen()
     even_fibs = (n for n in fibs if n % 2 == 0)
     small_even_fibs = filter(lambda x: x < 4000000, even_fibs)
     total = sum(small_even_fibs)
-    print(total)
+    return total
+
+def solve_2():
+    ans = sum(
+            x for x in
+            itertools.takewhile(
+                lambda x: x < 4_000_000,
+                fibonacci_gen()
+                )
+            if x % 2 == 0
+            )
+    return ans
+
+if __name__ == '__main__':
+    print(solve_1())
+    print(solve_2())
